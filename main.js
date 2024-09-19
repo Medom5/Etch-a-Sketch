9655da9e24bfbe1fc79ba9a2f7462362cab32af7
Medom05 <145607268+Medom5@users.noreply.github.com>
2024-09-19 08:36:16 +0300

    create: 16*16 template grid with some styling

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let container = document.getElementsByClassName("container");
+let container = document.getElementsByClassName("container")[0];
+let contH = 500;
+let contW = 500;
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    div.classList.add("grid-div");
+    div.style.height = contH / 16 + "px";
+    div.style.width = contW / 16 + "px";
+    container.append(div);
+  }
+}
