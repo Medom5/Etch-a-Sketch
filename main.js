031235d9d516b8a209227619501cf1515d779c51
Medom05 <145607268+Medom5@users.noreply.github.com>
2024-09-19 10:47:47 +0300

    add: change size button with function on click and generateGrid with some code optimizations

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const contH = 500;
-const contW = 500;
+const contH = 600;
+const contW = 600;
 let container = document.getElementsByClassName("container")[0];
 container.style.cssText = `width : ${contW}px; height : ${contH}px;`;
 
+generateGrid(16);
 
-
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    div.classList.add("grid-div");
-    div.style.height = contH / 16 + "px";
-    div.style.width = contW / 16 + "px";
-      div.addEventListener("mouseenter", () => { div.style.backgroundColor = "black"; } )
-      div.addEventListener("click", () => div.style.backgroundColor ="")
-      container.append(div);
-  }
+function generateGrid(n) {
+    container.innerHTML = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let div = document.createElement("div");
+            div.classList.add("grid-div");
+            div.style.height = contH / n + "px";
+            div.style.width = contW / n + "px";
+            div.addEventListener("mouseenter", () => { div.style.backgroundColor = "black"; })
+            div.addEventListener("click", () => div.style.backgroundColor = "")
+            container.append(div);
+        }
+    }
 }
+
+let button = document.getElementById("size");
+button.addEventListener("click", () => {
+    let size = window.prompt("Enter the size (e.g. 16 will be 16*16 grid)")
+    if (size > 0 && size <= 100)
+        generateGrid(size);
+})
+
